785788aee43494d7bba6582eb5220a7cef0d8204
e-naldo <88638321+e-naldo@users.noreply.github.com>
2023-02-04 14:18:59 -0300

    Commit Inicial

---
+++ src/main/java/dev/controle/patrimonio/ControlePatrimonioApplication.java
@@ -0,0 +1,13 @@
+package dev.controle.patrimonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePatrimonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePatrimonioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/controle/patrimonio/ControlePatrimonioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.controle.patrimonio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePatrimonioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
