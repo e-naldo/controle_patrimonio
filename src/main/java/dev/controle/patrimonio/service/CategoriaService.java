d872bc082e8932440affc8f2d026987cb8368fa5
e-naldo <88638321+e-naldo@users.noreply.github.com>
2023-02-05 16:50:28 -0300

    Add CategoriaService

---
+++ src/main/java/dev/controle/patrimonio/service/CategoriaService.java
@@ -0,0 +1,48 @@
+package dev.controle.patrimonio.service;
+
+import dev.controle.patrimonio.model.Categoria;
+import dev.controle.patrimonio.repository.CategoriaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaService implements GenericService<Categoria>{
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    @Override
+    public Categoria cadastrar(Categoria categoria) {
+        return repository.save(categoria);
+    }
+
+    @Override
+    public Categoria atualizar(Categoria categoria) {
+        Categoria categoriaAtual = repository.findById(categoria.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        categoriaAtual.setNome(categoria.getNome());
+        repository.save(categoria);
+
+        return categoriaAtual;
+    }
+
+    @Override
+    public Categoria buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public List<Categoria> buscarTodos() {
+        return repository.findAll();
+    }
+
+    @Override
+    public void excluirPorId(Long id) {
+            repository.deleteById(id);
+    }
+}
